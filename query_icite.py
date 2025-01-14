cd830139f84f1dfff5f2b63499cd05238aa23a51
Adrienne Stilp <amstilp@uw.edu>
2025-01-14 14:36:17 -0800

    Write the method to pull iCite pubs records

--- query_icite.py
+++ query_icite.py
@@ -5,11 +5,9 @@ import os
 import pandas as pd
 import requests
 
-def write_to_json(results, filename):
-    # Note: you can recreate the model instance with Model(**record.to_dict())
-    # eg: Score(**score.to_dict())
+def write_to_json(x, filename):
     with open(filename, "w") as f:
-        f.write(json.dumps([x.to_dict() for x in results], default=str, indent=2))
+        f.write(json.dumps(x, default=str, indent=2))
 
 
 def create_icite_analysis(pmids):
@@ -37,7 +35,10 @@ def get_icite_records(pmids):
     Returns:
         list: json data for each PMID from iCite.
     """
-    return []
+    icite_url = "https://icite.od.nih.gov/api/pubs"
+    data = {"pmids": ",".join(["{}".format(x) for x in pmids])}
+    response = requests.get(icite_url, params=data)
+    return response.json()["data"]
 
 
 if __name__ == "__main__":
@@ -66,6 +67,7 @@ if __name__ == "__main__":
     print("Pulling iCite records for PMIDs...")
     icite_records = get_icite_records(pmids)
 
+    # import ipdb; ipdb.set_trace()
     # Write output to files.
     os.makedirs(args.outdir, exist_ok=True)
     with open(os.path.join(args.outdir, "icite_search_id.txt"), "w") as f:
